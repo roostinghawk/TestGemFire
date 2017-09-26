78c6a8a9606af1cc3c81e44c1a3141b585c673a3
wei.liu <liuw198606@aliyun.com>
2017-09-26 14:58:18 +0800

    no message

---
+++ src/main/java/PersonRepository.java
@@ -0,0 +1,13 @@
+
+import org.springframework.data..CrepositoryrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, String> {
+
+    Person findByName(String name);
+
+    Iterable<Person> findByAgeGreaterThan(int age);
+
+    Iterable<Person> findByAgeLessThan(int age);
+
+    Iterable<Person> findByAgeGreaterThanAndAgeLessThan(int age1, int age2);
+}
